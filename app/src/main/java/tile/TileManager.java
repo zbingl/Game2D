c2222a4e391af8eb2b51e96d4b7ed44a33db9d6d
zbingl <zbingl1234@gmail.com>
2025-10-09 23:18:10 +0200

    wall tiles

--- app/src/main/java/tile/TileManager.java
+++ app/src/main/java/tile/TileManager.java
@@ -22,7 +22,7 @@ public class TileManager {
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
-        tile = new Tile[10];
+        tile = new Tile[20];
         getTileImages();
         mapList = new TreeMap<String, GameMap>();
         mapList.put("house main", new GameMap("/res/maps/houseMain.txt", 28, 14, gp));
@@ -57,6 +57,67 @@ public class TileManager {
 
             tile[6] = new Tile();
             tile[6].image = ImageIO.read(getClass().getResource("/res/tiles/earth.png"));
+
+
+
+            //Walls (border only)
+            tile[7] = new Tile();
+            tile[7].image = ImageIO.read(getClass().getResource("/res/tiles/walls/leftBottom.png"));
+            tile[7].collision = true;
+
+            tile[8] = new Tile();
+            tile[8].image = ImageIO.read(getClass().getResource("/res/tiles/walls/rightBottom.png"));
+            tile[8].collision = true;
+
+            tile[9] = new Tile();
+            tile[9].image = ImageIO.read(getClass().getResource("/res/tiles/walls/left.png"));
+            tile[9].collision = true;
+
+            tile[10] = new Tile();
+            tile[10].image = ImageIO.read(getClass().getResource("/res/tiles/walls/bottom.png"));
+            tile[10].collision = true;
+
+            tile[11] = new Tile();
+            tile[11].image = ImageIO.read(getClass().getResource("/res/tiles/walls/right.png"));
+            tile[11].collision = true;
+
+            //Walls
+
+            tile[12] = new Tile();
+            tile[12].image = ImageIO.read(getClass().getResource("/res/tiles/walls/bottomWall.png"));
+            tile[12].collision = true;
+
+            tile[13] = new Tile();
+            tile[13].image = ImageIO.read(getClass().getResource("/res/tiles/walls/leftBottomWall.png"));
+            tile[13].collision = true;
+
+            tile[14] = new Tile();
+            tile[14].image = ImageIO.read(getClass().getResource("/res/tiles/walls/rightBottomWall.png"));
+            tile[14].collision = true;
+
+            tile[15] = new Tile();
+            tile[15].image = ImageIO.read(getClass().getResource("/res/tiles/walls/topWall.png"));
+            tile[15].collision = true;
+
+            tile[16] = new Tile();
+            tile[16].image = ImageIO.read(getClass().getResource("/res/tiles/walls/rightTopWall.png"));
+            tile[16].collision = true;
+
+            tile[17] = new Tile();
+            tile[17].image = ImageIO.read(getClass().getResource("/res/tiles/walls/leftTopWall.png"));
+            tile[17].collision = true;
+
+            tile[18] = new Tile();
+            tile[18].image = ImageIO.read(getClass().getResource("/res/tiles/walls/leftWall.png"));
+            tile[18].collision = true;
+
+            tile[19] = new Tile();
+            tile[19].image = ImageIO.read(getClass().getResource("/res/tiles/walls/rightWall.png"));
+            tile[19].collision = true;
+
+
+
+
 
 
             
